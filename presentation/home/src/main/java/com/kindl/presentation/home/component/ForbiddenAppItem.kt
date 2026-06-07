b798f942a01fa69bdcafbd86fec69496615bf0ea
sonms <smsms5676@naver.com>
2026-06-08 08:50:04 +0900

    [Refactor#7] 접근성 권한 설정 수정 및 ForbiddenAppItem UI 컴포넌트 개선
    - `PermissionType`에서 `ACCESSIBILITY` 권한의 필수 여부(`isFocusRequired`)를 `true`에서 `false`로 변경하였습니다.
    - `ForbiddenAppItem` 컴포넌트의 이미지 로딩 방식을 `UrlImage`에서 `Image` 및 `Bitmap` 변환 방식으로 변경하고 `ContentScale.Crop`을 적용하였습니다.
    - `ForbiddenAppItem` 내 앱 명칭(`appName`)의 텍스트 스타일과 색상을 `KindlTheme` 가이드라인에 맞춰 업데이트하였습니다.
    - `HomeViewModel`에 `Timber` 로그 라이브러리 import를 추가하였습니다.

--- presentation/home/src/main/java/com/kindl/presentation/home/component/ForbiddenAppItem.kt
+++ presentation/home/src/main/java/com/kindl/presentation/home/component/ForbiddenAppItem.kt
@@ -1,5 +1,6 @@
 package com.kindl.presentation.home.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
@@ -9,8 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
 import com.kindl.core.designsystem.component.UrlImage
 import com.kindl.core.designsystem.theme.KindlTheme
 import com.kindl.presentation.home.model.ForbiddenAppUiModel
@@ -24,16 +28,26 @@ internal fun ForbiddenAppItem(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        UrlImage(
-            url = item.icon,
-            modifier = Modifier
-                .size(36.dp)
-                .clip(RoundedCornerShape(16.dp))
-        )
+        val iconModifier = Modifier
+            .size(36.dp)
+            .clip(RoundedCornerShape(16.dp))
+
+        item.icon?.toBitmap()?.asImageBitmap()?.let {
+            Image(
+                bitmap = it,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = iconModifier
+            )
+        }
 
         Spacer(modifier = Modifier.weight(1f))
 
-        Text(text = item.appName)
+        Text(
+            text = item.appName,
+            style = KindlTheme.typography.semiBold.title3,
+            color = KindlTheme.colors.primary
+        )
     }
 }
 
